6d825c4429a3afef581197742b0ebb01870b7f2f
TATSIANA PRADUKHA <tatyanaproduho@mail.ru>
2024-09-26 12:33:11 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -58,6 +58,9 @@ class Dev(Configuration):
     CELERY_RESULT_BACKEND = "django-db"
     CELERY_BROKER_URL = "redis://localhost:6379/0"
 
+    #use console email backend
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Ben", "ben@example.com")]
     
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
